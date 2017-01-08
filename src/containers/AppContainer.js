4295bb41104749b30d99328d9533c07737835d94
Alexander Gusev <inbox@goooseman.ru>
2017-01-08 17:44:19 +0200

    [NEW] google analytics

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,13 +1,21 @@
 import React, { Component, PropTypes } from 'react';
 import { Router } from 'react-router';
 import { Provider } from 'react-redux';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-90024802-1');
+
+const logPageView = () => {
+  ReactGA.set({page: window.location.pathname});
+  ReactGA.pageview(window.location.pathname);
+};
 
 class AppContainer extends Component {
   static propTypes = {
     history: PropTypes.object.isRequired,
     routes: PropTypes.object.isRequired,
     store: PropTypes.object.isRequired
-  }
+  };
 
   render () {
     const { history, routes, store } = this.props;
@@ -15,7 +23,7 @@ class AppContainer extends Component {
     return (
       <Provider store={store}>
         <div style={{ height: '100%', overflow: 'hidden' }}>
-          <Router history={history} children={routes} />
+          <Router history={history} onUpdate={logPageView} children={routes} />
         </div>
       </Provider>
     );
