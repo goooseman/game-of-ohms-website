554b46da822e956ebedc9edd19a5c3fe04638873
Alexander Gusev <inbox@goooseman.ru>
2017-01-08 17:15:27 +0200

    [FIX] playing on iOS

--- src/components/IndexSlideOne/IndexSlideOne.js
+++ src/components/IndexSlideOne/IndexSlideOne.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './IndexSlideOne.scss';
 import bg from './assets/pattern.png';
 import logo from 'images/logo.svg';
 
-export const IndexSlideOne = () => (
-  <div className={classes['container']} style={{backgroundImage: `url(${bg})`}}>
-    <div className={classes['bg']}>
-      <video id='index-video' autoPlay muted loop poster='/bg.jpg'>
-        <source src='/bg.mp4' type='video/mp4' />
-        <source src='/bg.webm' type='video/webm' />
-      </video>
-    </div>
-    <div className={classes['center']}>
-      <img src={logo} alt='Logo' />
-      <p>Super Duper Premium E-Liquids</p>
-    </div>
-    <div className={classes['bottom']}>
-      <p>Made with ❤️ in Eilat</p>
-    </div>
-  </div>
-);
+class IndexSlideOne extends Component {
+
+  componentDidMount () {
+    this.refs.video.play();
+  };
+
+  render () {
+    return (
+      <div className={classes['container']} style={{backgroundImage: `url(${bg})`}}>
+        <div className={classes['bg']}>
+          <video ref='video' playsInline muted='true' loop poster='/bg.jpg'>
+            <source src='/bg.mp4' type='video/mp4' />
+            <source src='/bg.webm' type='video/webm' />
+          </video>
+        </div>
+        <div className={classes['center']}>
+          <img src={logo} alt='Logo' />
+          <p>Super Duper Premium E-Liquids</p>
+        </div>
+        <div className={classes['bottom']}>
+          <p>Made with ❤️ in Eilat</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default IndexSlideOne;
